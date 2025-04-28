c5a15f0916eb12b20b5311ee365ce07c16659571
pablorubiomonzo0330 <pablo.rubiomonzo@otto.de>
2025-04-28 19:28:48 +0200

    Added comments

--- src/App.tsx
+++ src/App.tsx
@@ -11,11 +11,14 @@ interface InformationNewSubnets {
 
 
 function App() {
+
+    // Information needed to render automatically the website after any change with React
   const [ipAddress, setIpAddress] = useState("192.168.1.0");
   const [subnetMask, setSubnetMask] = useState("255.255.255.0");
   const [desiredSubnets, setDesiredSubnets] = useState("4");
   const [subnetsIpInformation, setSubnetsIpInformation] = useState<InformationNewSubnets[]>([]);
 
+  // Functions needed to update the state of every input field and keep track on what is written
     const handleChangeIpAddress = (event: ChangeEvent<HTMLInputElement>) =>{
         setIpAddress(event.target.value)
     }
@@ -35,18 +38,22 @@ function App() {
     }
 
     function calculateSubnets(){
+
+        // Helper Function
         function ipToBinary(ip: string){
             return ip.split(".").map(octet => parseInt(octet, 10).toString(2).padStart(8, "0")).join("")
         }
-
+        // Helper Function
         function fromBinaryToDecimal(ipInBinary: string){
             const ipInOctets = new Array(4)
 
+            // Puts each octet in the array
             for( let i = 0; i < ipInOctets.length; i++){
                 ipInOctets[i] = parseInt(ipInBinary.slice(i * 8, (i + 1)*8), 2)
 
             }
 
+            // Transforms the array into a string separating each octet with a point
             return ipInOctets.join(".")
         }
 
@@ -55,39 +62,38 @@ function App() {
             return ipInBinary.split('').map((bit, index) => (bit === '1' && submaskBinary[index] === '1') ? "1" : "0").join("");
         }
 
-        const submaskBinary = ipToBinary(subnetMask)
-        const submaskCIDR = submaskBinary.split("").filter(bit => bit === "1").length
+        const subnetMaskBinary = ipToBinary(subnetMask)
+        const subnetMaskCIDR = subnetMaskBinary.split("").filter(bit => bit === "1").length
 
         const neededBits = Math.ceil(Math.log2(parseInt(desiredSubnets)))
-        const newSubMaskCIDR = submaskCIDR + neededBits
+        const newSubMaskCIDR = subnetMaskCIDR + neededBits
 
-        const newSubmaskInBinary = "1".repeat(newSubMaskCIDR) + "0".repeat(32 - newSubMaskCIDR)
+        const newSubnetMaskInBinary = "1".repeat(newSubMaskCIDR) + "0".repeat(32 - newSubMaskCIDR)
 
-        const newSubmaskInDecimal = fromBinaryToDecimal(newSubmaskInBinary)
+        const newSubnetMaskInDecimal = fromBinaryToDecimal(newSubnetMaskInBinary)
 
 
 
-        const networkIpInBinary = networkBinary(ipAddress, submaskBinary)
+        const networkIpInBinary = networkBinary(ipAddress, subnetMaskBinary)
         console.log(networkIpInBinary)
-        const networkAddressWithoutHostBits = networkIpInBinary.slice(0, 32 - (32 - submaskCIDR))
+        const networkAddressWithoutHostBits = networkIpInBinary.slice(0, 32 - (32 - subnetMaskCIDR))
 
+        // This functions create the needed extra bits between the network part and the host part of the IP
         function generateBinaryStrings(neededBits: number) {
             const binaryStrings = [];
-            const maxNumber = Math.pow(2, neededBits); // Calculate the maximum number for the given bits
+            const maxNumber = Math.pow(2, neededBits);
 
             for (let i = 0; i < maxNumber; i++) {
-                const binaryString = i.toString(2).padStart(neededBits, '0'); // Convert to binary and pad with zeros
+                const binaryString = i.toString(2).padStart(neededBits, '0');
                 binaryStrings.push(binaryString);
             }
-
             return binaryStrings;
         }
 
-        console.log(submaskCIDR)
-        console.log(newSubMaskCIDR)
         const subnetsToPush = []
+
+        // Creates the information through a loop for every single subnet and pushes it into subnetsToPush
         for (let i = 0; i < Number(desiredSubnets); i++){
-            console.log("network address is: ", networkAddressWithoutHostBits)
             const ipGateway = fromBinaryToDecimal(networkAddressWithoutHostBits + generateBinaryStrings(neededBits)[i] + "0".repeat(32 - newSubMaskCIDR))
             const ipBroadcastDirection = fromBinaryToDecimal(networkAddressWithoutHostBits + generateBinaryStrings(neededBits)[i] + "1".repeat(32 - newSubMaskCIDR))
             const ipFirstAvailableDirection = fromBinaryToDecimal(networkAddressWithoutHostBits + generateBinaryStrings(neededBits)[i] + "0".repeat(32 - newSubMaskCIDR - 1) + "1")
@@ -96,37 +102,30 @@ function App() {
             subnetsToPush.push({
                 ipGateway,
                 ipBroadcastDirection,
-                subnetMask: newSubmaskInDecimal,
+                subnetMask: newSubnetMaskInDecimal,
                 ipFirstAvailableDirection,
                 ipLastAvailableDirection
             })
-            // Extract host bits from the network address in binary
-            // add the available hosts and needed padding calculations in binary
-            console.log(`-------- Subnet ${i + 1} ------------`)
-            console.log("ipGateway in binary: ", networkAddressWithoutHostBits + "/" + generateBinaryStrings(neededBits)[i] + "/" + "0".repeat(32 - newSubMaskCIDR))
-            console.log("ip broadcast: ",networkAddressWithoutHostBits + "/" + generateBinaryStrings(neededBits)[i] + "/" + "1".repeat(32 - newSubMaskCIDR))
-            console.log("ip first Ip possible: ", networkAddressWithoutHostBits + "/" + generateBinaryStrings(neededBits)[i] + "/" + "0".repeat(32 - newSubMaskCIDR - 1) + "1")
-            console.log("ip last ip possible:", networkAddressWithoutHostBits.length + "/" + generateBinaryStrings(neededBits)[i].length + "/" + "1".repeat(32 - newSubMaskCIDR - 1) + "0")
-            console.log("ip subnet submask: ", )
-            // show the information in dotted decimal
         }
         setSubnetsIpInformation(subnetsToPush)
 
     }
 
+    // Renders the information in a card when called
   const renderIpInformation = () => {
-        return subnetsIpInformation.slice(-4, subnetsIpInformation.length).map((ipInformation, index) => {
+        return subnetsIpInformation.slice(-desiredSubnets, subnetsIpInformation.length).map((ipInformation, index) => {
             return <div className="subnet-card" key={index}>
                 <h2>Subnet {index + 1}</h2>
                 <p><strong>IPs Gateway:</strong> {ipInformation.ipGateway}</p>
-                <p><strong>IPs Broadcast Direction:</strong> {ipInformation.ipBroadcastDirection}</p>
-                <p><strong>IPs Subnet Mask:</strong> {ipInformation.subnetMask}</p>
                 <p><strong>IPs First Available Direction:</strong> {ipInformation.ipFirstAvailableDirection}</p>
                 <p><strong>IPs Last Available Direction:</strong> {ipInformation.ipLastAvailableDirection}</p>
+                <p><strong>IPs Broadcast Direction:</strong> {ipInformation.ipBroadcastDirection}</p>
+                <p><strong>IPs Subnet Mask:</strong> {ipInformation.subnetMask}</p>
             </div>
         })
   }
 
+    // Renders the whole application
     return (
         <div className="main-container">
             <img className="img-logo" alt="logo" src={Logo}/>
